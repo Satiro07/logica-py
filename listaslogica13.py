9bd79c76abb548ac6f48347efd076dd7c00c9490
Satiro07 <jardelsatiro57@gmail.com>
2025-03-13 19:56:53 -0300

    Update listaslogica13.py

--- listaslogica13.py
+++ listaslogica13.py
@@ -1,12 +1,15 @@
 lista1 = [2, 5, 'abril', 'amanha', 33]
 lista = lista1
+
 lista2 = [1, 2, 3, 4, 5]
 lista22 = lista2
-
+lista12 = lista2
 for i in range(5):
-    lista1[i] = lista22[i]
+    
+    lista12[i] = lista1[i]
 for i in range(5):
-    lista2[i] = lista[i]
+    lista1[i] = lista12[i]
+    lista2[i] = lista12[i]
     
     
 print(lista1)
