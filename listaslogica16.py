cd21839cb60048c15f126ccda05e37fa29cc9335
Satiro07 <jardelsatiro57@gmail.com>
2025-03-14 20:49:51 -0300

    Update listaslogica16.py

--- listaslogica16.py
+++ listaslogica16.py
@@ -1,19 +1,16 @@
-def verificar(lista, a):
-    
-    c = 0
-    r = 0
-    tot = len(lista)
-    for i in range(0, tot):
-        if a != lista[i]:
-            r += 1
-        else:
-            lista.remove(a)
-        tot = len(lista)
-        c += 1
-    return lista
 
 
 
 lista = [2, 5, 'abril', 'amanha',33, 1, 3, 4, 6, 6]
 a = input('Digite um elemento: ')
-print(verificar(lista, a))
+
+tot = len(lista)
+r = 0
+for i in range(0, tot):
+    if a != lista[i]:
+        r += 1
+    else:
+        lista.remove(a)
+    tot = len(lista)
+        
+print(lista)
