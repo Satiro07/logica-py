8121eaaa9be7061180e2c867a8bf04d23687f16f
Satiro07 <jardelsatiro57@gmail.com>
2025-03-14 21:03:10 -0300

    .

--- listaslogica16.py
+++ listaslogica16.py
@@ -1,16 +1,18 @@
 
 
 
-lista = [2, 5, 'abril', 'amanha',33, 1, 3, 4, 6, 6]
+lista = [2, 5, 'abril', 'abril', 33, 1, 3, 4, 6, 6]
 a = input('Digite um elemento: ')
 
+
+c = 0
 tot = len(lista)
-r = 0
-for i in range(0, tot):
-    if a != lista[i]:
-        r += 1
-    else:
+
+while c <= tot-1:
+    if a == str(lista[c]):
         lista.remove(a)
+        c -= 1
     tot = len(lista)
+    c += 1
         
 print(lista)
