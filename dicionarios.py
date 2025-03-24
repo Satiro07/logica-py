7d2247d39d92ea31d4abe1eae3d6315715deaff6
Satiro07 <jardelsatiro57@gmail.com>
2025-03-23 21:41:57 -0300

    Update dicionarios.py

--- dicionarios.py
+++ dicionarios.py
@@ -1,14 +1,15 @@
 geral = []
-sistema = {}
+
 
 for i in range(0, 2):
+    sistema = {}
     notas = []
-    sistema['nome'] = input('Nome do aluno: ')
+    nome = input('Nome do aluno: ')
     c = 0
     while c < 3:
         nota = float(input('Nota do aluno: '))
         notas.append(nota)
         c += 1
-    sistema['nome'] = notas
-    geral.append*sistema
+    sistema[nome] = notas
+    geral.append(sistema)
 print(geral)
