bd474b0a90299a3d388df64f055e6c0fefe5a9b3
Satiro07 <jardelsatiro57@gmail.com>
2025-03-25 11:14:42 -0300

    Update dicionarios1.py

--- dicionarios1.py
+++ dicionarios1.py
@@ -1,7 +1,24 @@
-lista = []
-frase = input('Digite uma frase: ').strip().split()
-cont = 0
-for palavra in frase:
-    cont += 1
 
-print(f'A frase {frase.join("-")} tem {cont} palavras')
+lista = []
+
+while True:
+    frases = ''
+    cont = []
+    geral = []
+    dicionario = {}
+    frase1 = 'n'
+    while frase1 != 's':
+        frase1 = input('Digite uma frase ou "s" para sair: ').strip()
+        frase = frase1.split()
+        frases = frase
+        cont += frases
+        print(frases)
+    for palavra in frase:
+        palavra_nome = palavra
+        pala = frase.count(palavra)
+        dicionario[palavra_nome] = pala
+    geral.append(dicionario)
+    lista.append(geral)
+    break
+    
+print(lista)
