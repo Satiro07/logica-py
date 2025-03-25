537e4a783fb6286355f7ac8335b4229b249a11b2
Satiro07 <jardelsatiro57@gmail.com>
2025-03-25 10:55:11 -0300

    Update dicionarios1.py

--- dicionarios1.py
+++ dicionarios1.py
@@ -1,4 +1,7 @@
 lista = []
-palavra = input('Digite uma frase: ').strip().split()
-print(palavra[0])
+frase = input('Digite uma frase: ').strip().split()
+cont = 0
+for palavra in frase:
+    cont += 1
 
+print(f'A frase {frase.join("-")} tem {cont} palavras')
