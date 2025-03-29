38c702bfdbd6510adfa19d83dbd0d54439c8ff58
Satiro07 <jardelsatiro57@gmail.com>
2025-03-29 10:37:59 -0300

    Update log.py

--- log.py
+++ log.py
@@ -1,16 +1,15 @@
-# def contar_vogais(texto):
-#     contador = 0
-#     for palavra in texto:
-#         for letra in palavra:
-#             if letra == 'a' or letra == 'e' or letra == 'i' or letra == 'o' or letra == 'u':
-#                 contador += 1
-#     return contador
-        
+def contar_vogais(texto):
+    vogais = 'aeiouáéíóúãâêîôûõàèìòù'
+    contador = 0
+    for palavra in texto:
+        for letra in palavra:
+            if letra in vogais:
+                cont = letra.count(letra)
+                contador += cont
+    return contador
 
-
-# frase = input('Digite algo: ').lower().split()
-# print(frase)
-# resp = contar_vogais(frase)
-# print(resp)
-letra = 'é'
-print(letra.remove('´'))
+frase = input('Digite algo: ').lower().split()
+resp = contar_vogais(frase)
+print(resp)
+# letra = 'é'
+# print(letra.remove('´'))
