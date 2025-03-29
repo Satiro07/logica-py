c5d33fa2fe7b0c68efc86d3f0eb9c5689f0099b0
Satiro07 <jardelsatiro57@gmail.com>
2025-03-29 10:33:01 -0300

    Create log.py

---
+++ log.py
@@ -0,0 +1,16 @@
+# def contar_vogais(texto):
+#     contador = 0
+#     for palavra in texto:
+#         for letra in palavra:
+#             if letra == 'a' or letra == 'e' or letra == 'i' or letra == 'o' or letra == 'u':
+#                 contador += 1
+#     return contador
+        
+
+
+# frase = input('Digite algo: ').lower().split()
+# print(frase)
+# resp = contar_vogais(frase)
+# print(resp)
+letra = 'é'
+print(letra.remove('´'))
