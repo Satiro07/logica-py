fb5708e4229fe1b7e08590272f7f85816b951e2d
Satiro07 <jardelsatiro57@gmail.com>
2025-05-26 21:51:15 -0300

    Update logi.py

--- logica/logi.py
+++ logica/logi.py
@@ -3,14 +3,13 @@ pontos = ',.;:!?'
 frase = input('Digite uma frase: ').lower()
 palavras = frase.split()
 novas_palavras = []
+contagem = 0
 for palavra in palavras:
-    verificacao = False
-    palavra_completa = palavra
-    for i in range(len(palavra)):
-        if palavra[i] in pontos:
-            palavra_completa.remove(palavra_completa[i])
-            novas_palavras.append(palavra_completa)
-            verificacao = True
-    if verificacao == False:
-        novas_palavras.append(palavra_completa)
+    palavra_frag = palavra.split()
+    cont = len(palavra_frag)
+    palavra_certa = []
+    for i in range(cont):
+        if palavra_frag[i] not in pontos:
+            palavra_certa.append(palavra_frag[i])
+    novas_palavras.append(palavra_certa)
 print(novas_palavras)
